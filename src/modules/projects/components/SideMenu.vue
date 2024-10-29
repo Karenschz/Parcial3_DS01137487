24c85db865ee58cb357ea0fac6ca3cd61254bf9a
Karenschz <karenschz27@gmail.com>
2024-10-28 20:36:49 -0600

    Add files via upload

---
+++ src/modules/projects/components/SideMenu.vue
@@ -0,0 +1,6 @@
+<template>
+  <aside class="bg-base-200 w-72 min-h-screen">
+    <h2 class="text-lg font-bold mx-4">Proyecto</h2>
+    <p class="text-sm text-gray-500 mx-4">No hay proyectos</p>
+  </aside>
+</template>
---
+++ src/modules/projects/layouts/ProjectsLayouts.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex flex-col">
+    <!--Top Menu-->
+    <top-menu />
+
+    <main class="flex flex-row">
+      <!--Side Menu-->
+      <side-menu />
+
+      <ProjectView />
+      <!-- RouterView-->
+      <router-view />
+    </main>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import TopMenu from '@/modules/common/components/TopMenu.vue';
+
+import SideMenu from '../components/SideMenu.vue';
+</script>
